0018a3f741c7a5baf0c471ef396784951a463eef
Michael Umeokoli <michaeljunior794@gmail.com>
2022-04-04 19:51:54 +0100

    'projects-section'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Contact from "./components/Contact";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
-import ProjectCard from "./UI/ProjectCard";
 
 function App() {
   return (
