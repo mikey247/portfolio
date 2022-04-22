640322a8f54fb22965c7cffcd591b86d1fbe4884
Michael Umeokoli <michaeljunior794@gmail.com>
2022-04-22 13:34:57 +0100

    'added-descriptions'

--- src/data.js
+++ src/data.js
@@ -3,45 +3,45 @@ export const projects = [
     title: "Mike's Meals",
     github: "React",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: 'assets/mikemeals.gif',
+      "A food order app with an interactive cart, I made use of React tools such as useContext anssd useReducer to be able pass and manipulate state through out the appplication ",
+    image: "assets/mikemeals.gif",
     link: "https://calm-pithivier-b7e775.netlify.app/",
-    tools: ['React' , 'Context-API','useReducer' ,'CSS Modules']
+    tools: ["React", "Context-API", "useReducer", "CSS Modules"],
   },
-    {
-      title: "Country-Frenzy",
-      github: "React",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: 'assets/countryfrenzy.gif',
-      link: "http://country-frenzy.netlify.app/",
-      tools: ['React' , 'Rest API', 'JavaScript']
-    },
-    {
-      title: "Mike's Desk",
-      github: "React and Python",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: 'assets/Mike-Desk.gif',
-        link: "https://mike-s-desk.herokuapp.com/",
-        tools: [ 'django' , 'python', 'AWS S3', 'heroku', 'CKEditor']
-    },
-    {
-      title: "Advice Generator",
-      github: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: 'assets/advice-generator.gif',
-        link: "https://mike-s-advice-generator.netlify.app/",
-        tools: ['JavaScript', 'Rest API','HTML', 'CSS']
-    },
-    {
-      title: "TradExpress",
-      github: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: 'assets/TradExpress.gif',  
-       link: "https://gallant-carson-667c21.netlify.app/",
-    tools: ['React','JavaScript', 'HTML', 'CSS']
-    },
-  ];
+  {
+    title: "Country-Frenzy",
+    github: "React",
+    description:
+      "A project built for my kid brother Samuel (he loves geography and general world info),It shows information on all the countries of the world e.g currencies, language, capitals, continent etc. I manipulated the Rest countries API to create the project, add search functionality and other features in the app.",
+    image: "assets/countryfrenzy.gif",
+    link: "http://country-frenzy.netlify.app/",
+    tools: ["React", "Rest API", "JavaScript"],
+  },
+  {
+    title: "Mike's Desk",
+    github: "React and Python",
+    description:
+      "A personal blog I built whilst dabbling in Django, comes with a database, administration panel and comment system for user interaction and static files hosted on AWS S3  ",
+    image: "assets/Mike-Desk.gif",
+    link: "https://mike-s-desk.herokuapp.com/",
+    tools: ["django", "python", "AWS S3", "heroku", "CKEditor", "SQLite"],
+  },
+  {
+    title: "Advice Generator",
+    github: "React and Firebase",
+    description:
+      "An advice generator built with an API that fetches you a piece of advice on the click of the dice",
+    image: "assets/advice-generator.gif",
+    link: "https://mike-s-advice-generator.netlify.app/",
+    tools: ["JavaScript", "Rest API", "HTML", "CSS"],
+  },
+  {
+    title: "TradExpress(Under Construction🛠)",
+    github: "React Hooks",
+    description:
+      "A crypto-currency trading site site built with React and react-router ",
+    image: "assets/TradExpress.gif",
+    link: "https://gallant-carson-667c21.netlify.app/",
+    tools: ["React", "JavaScript", "HTML", "CSS"],
+  },
+];
