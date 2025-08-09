7a9f4e31257742f39dd92877a01736a3f671cff4
Michael Umeokoli <michaeljunior794@gmail.com>
2025-08-09 16:51:58 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -17,15 +17,15 @@ export const projects = [
     link: "https://linqman.com",
     tools: ["Next.js", "React.js", "CSS Modules", "Nodejs", "Express"],
   },
-  {
-    title: "NATOURS.(under-construction🔨)",
-    github: "https://github.com/mikey247/Natours-React",
-    description:
-      "A tour/vacation business for lovers of nature.This React app compliments an API built by myself with Nodejs/Express, Redux toolkit will be used for state management and JWT authentication will be implemented",
-    image: "assets/natours.gif",
-    link: "https://natours-explore.netlify.app",
-    tools: ["React", "CSS Modules", "Nodejs", "Express", "MongoDB", "Mongoose"],
-  },
+  // {
+  //   title: "NATOURS.(under-construction🔨)",
+  //   github: "https://github.com/mikey247/Natours-React",
+  //   description:
+  //     "A tour/vacation business for lovers of nature.This React app compliments an API built by myself with Nodejs/Express, Redux toolkit will be used for state management and JWT authentication will be implemented",
+  //   image: "assets/natours.gif",
+  //   link: "https://natours-explore.netlify.app",
+  //   tools: ["React", "CSS Modules", "Nodejs", "Express", "MongoDB", "Mongoose"],
+  // },
   {
     title: "THE STORE.(under-construction🔨)",
     github: "https://github.com/mikey247/THE_STORE.",
@@ -44,57 +44,57 @@ export const projects = [
       "Mongoose",
     ],
   },
-  {
-    title: "Todo Buddy",
-    github: "https://github.com/mikey247/Todo-Buddy",
-    description:
-      "A todo list app with sign-up/sign-in capabilities using firebase email and password authentication with data stored on firestore database",
-    image: "assets/todo-buddy.gif",
-    link: "https://todo-buddy.netlify.app/",
-    tools: [
-      "React",
-      "Context-API",
-      "Firestore",
-      "CSS Modules",
-      "Firebase Auth",
-    ],
-  },
-  {
-    title: "Mike's Meals",
-    github: "https://github.com/mikey247/mike-s-meals",
-    description:
-      "A food order app with an interactive cart, I made use of React tools such as useContext anssd useReducer to be able pass and manipulate state through out the appplication ",
-    image: "assets/mikemeals.gif",
-    link: "https://calm-pithivier-b7e775.netlify.app/",
-    tools: ["React", "Context-API", "useReducer", "CSS Modules", "Firebase"],
-  },
-  {
-    title: "Country-Frenzy",
-    github: "https://github.com/mikey247/Country-Frenzy",
-    description:
-      "A project built for my kid brother Samuel (he loves geography and general world info),It shows information on all the countries of the world e.g currencies, language, capitals, continent etc. I manipulated the Rest countries API to create the project, add search functionality and other features in the app.",
-    image: "assets/countryfrenzy.gif",
-    link: "http://country-frenzy.netlify.app/",
-    tools: ["React", "Rest API", "JavaScript"],
-  },
-  {
-    title: "Mike's Desk",
-    github: "https://github.com/mikey247/mike-s-desk-test",
-    description:
-      "A personal blog I built whilst dabbling in Django, comes with a database, administration panel and comment system for user interaction and static files hosted on AWS S3  ",
-    image: "assets/Mike-Desk.gif",
-    link: "https://mike-s-desk.herokuapp.com/",
-    tools: ["django", "python", "AWS S3", "heroku", "CKEditor"],
-  },
-  {
-    title: "Advice Generator",
-    github: "https://github.com/mikey247/advice-generator",
-    description:
-      "An advice generator built with an API that fetches you a piece of advice on the click of the dice",
-    image: "assets/advice-generator.gif",
-    link: "https://mike-s-advice-generator.netlify.app/",
-    tools: ["JavaScript", "Rest API", "HTML", "CSS"],
-  },
+  // {
+  //   title: "Todo Buddy",
+  //   github: "https://github.com/mikey247/Todo-Buddy",
+  //   description:
+  //     "A todo list app with sign-up/sign-in capabilities using firebase email and password authentication with data stored on firestore database",
+  //   image: "assets/todo-buddy.gif",
+  //   link: "https://todo-buddy.netlify.app/",
+  //   tools: [
+  //     "React",
+  //     "Context-API",
+  //     "Firestore",
+  //     "CSS Modules",
+  //     "Firebase Auth",
+  //   ],
+  // },
+  // {
+  //   title: "Mike's Meals",
+  //   github: "https://github.com/mikey247/mike-s-meals",
+  //   description:
+  //     "A food order app with an interactive cart, I made use of React tools such as useContext anssd useReducer to be able pass and manipulate state through out the appplication ",
+  //   image: "assets/mikemeals.gif",
+  //   link: "https://calm-pithivier-b7e775.netlify.app/",
+  //   tools: ["React", "Context-API", "useReducer", "CSS Modules", "Firebase"],
+  // },
+  // {
+  //   title: "Country-Frenzy",
+  //   github: "https://github.com/mikey247/Country-Frenzy",
+  //   description:
+  //     "A project built for my kid brother Samuel (he loves geography and general world info),It shows information on all the countries of the world e.g currencies, language, capitals, continent etc. I manipulated the Rest countries API to create the project, add search functionality and other features in the app.",
+  //   image: "assets/countryfrenzy.gif",
+  //   link: "http://country-frenzy.netlify.app/",
+  //   tools: ["React", "Rest API", "JavaScript"],
+  // },
+  // {
+  //   title: "Mike's Desk",
+  //   github: "https://github.com/mikey247/mike-s-desk-test",
+  //   description:
+  //     "A personal blog I built whilst dabbling in Django, comes with a database, administration panel and comment system for user interaction and static files hosted on AWS S3  ",
+  //   image: "assets/Mike-Desk.gif",
+  //   link: "https://mike-s-desk.herokuapp.com/",
+  //   tools: ["django", "python", "AWS S3", "heroku", "CKEditor"],
+  // },
+  // {
+  //   title: "Advice Generator",
+  //   github: "https://github.com/mikey247/advice-generator",
+  //   description:
+  //     "An advice generator built with an API that fetches you a piece of advice on the click of the dice",
+  //   image: "assets/advice-generator.gif",
+  //   link: "https://mike-s-advice-generator.netlify.app/",
+  //   tools: ["JavaScript", "Rest API", "HTML", "CSS"],
+  // },
   // {
   //   title: "TradExpress(Under Construction🛠)",
   //   github: "https://github.com/mikey247/TradExpress-Clone-REACTs",
